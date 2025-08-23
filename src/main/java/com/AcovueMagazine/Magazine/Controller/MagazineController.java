0d62fa900bb4c2133383811bb49f72e9c672ea03
WhooGeek <rlarjswn0303@naver.com>
2025-08-23 18:18:45 +0900

    Setup Project

---
+++ src/main/java/com/AcovueMagazine/Magazine/Controller/MagazineController.java
@@ -0,0 +1,13 @@
+package com.AcovueMagazine.Magazine.Controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MagazineController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Spring!";
+    }
+}
