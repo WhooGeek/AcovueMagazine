d46c9bd20899df48b0d8ef96f212448b23b318c7
WhooGeek <rlarjswn0303@naver.com>
2025-08-27 19:31:13 +0900

    feat: DTO, Repository, Service 구현

---
+++ src/main/java/com/AcovueMagazine/User/Dto/LoginUserDto.java
@@ -0,0 +1,14 @@
+package com.AcovueMagazine.User.Dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class LoginUserDto {
+
+    private String loginId;
+    private String password;
+}
