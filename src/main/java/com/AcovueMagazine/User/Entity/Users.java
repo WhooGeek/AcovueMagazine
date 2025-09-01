e37f3eaa345a76f2a94578e4f35eeb51df92908b
WhooGeek <rlarjswn0303@naver.com>
2025-09-01 23:50:13 +0900

    BUG: 코드 중복 수정

--- src/main/java/com/AcovueMagazine/User/Entity/Users.java
+++ src/main/java/com/AcovueMagazine/User/Entity/Users.java
@@ -32,10 +32,6 @@ public class Users {
     @Enumerated(EnumType.STRING)
     private UserRoll userRoll;
 
-    @Column(name = "user_roll", nullable = false)
-    @Enumerated(EnumType.STRING)
-    private UserRoll user_roll;
-
     @Column(name = "user_status", nullable = false)
     @Enumerated(EnumType.STRING)
     private UserStatus userStatus;
